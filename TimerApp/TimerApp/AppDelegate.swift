98abe5ec901f56919beed76646281aafdffa813e
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2018-06-15 15:43:34 +0200

    Commited empty project

---
+++ TimerApp/TimerApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TimerApp
+//
+//  Created by Zarzecki, Matthias on 15.06.18.
+//  Copyright © 2018 Zarzecki, Matthias. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
---
+++ TimerApp/TimerApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Zarzecki, Matthias on 15.06.18.
+//  Copyright © 2018 Zarzecki, Matthias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
