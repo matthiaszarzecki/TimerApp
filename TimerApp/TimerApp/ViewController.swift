7cb0e28d6161df8ad4b5898d856adee7b1aeb48d
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2018-06-19 13:17:07 +0200

    added timer

--- TimerApp/TimerApp/ViewController.swift
+++ TimerApp/TimerApp/ViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var secondsElapsed = 0
+    var timer = Timer()
+    var isTimerRunning = false
+    var resumeTapped = false
+    
+    @IBOutlet weak var timerDisplay: UILabel!
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
+        pauseButton.isEnabled = false
         super.viewDidLoad()
     }
 
@@ -18,4 +29,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        pauseButton.isEnabled = true
+    }
+    
+    @objc func updateTimer() {
+        if secondsElapsed < 1 {
+            timer.invalidate()
+        } else {
+            secondsElapsed += 1
+            timerDisplay.text = timeString(time: TimeInterval(secondsElapsed))
+        }
+    }
+    
+    @IBAction func pauseButtonClicked(_ sender: Any) {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            self.resumeTapped = true
+            self.pauseButton.setTitle("Resume", for: .normal)
+        } else {
+            runTimer()
+            self.resumeTapped = false
+            self.pauseButton.setTitle("Pause", for: .normal)
+        }
+    }
+    
+    @IBAction func startButtonClicked(_ sender: Any) {
+        if isTimerRunning == false {
+            runTimer()
+            isTimerRunning = true
+            self.startButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+        timer.invalidate()
+        secondsElapsed = 0
+        timerDisplay.text = timeString(time: TimeInterval(secondsElapsed))
+        isTimerRunning = false
+        pauseButton.isEnabled = false
+        startButton.isEnabled = true
+    }
+    
+    func timeString(time: TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    }
 }
